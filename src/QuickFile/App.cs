926212690d8b4cdf5c12aa80f3da9e3726817bbd
Riley Beck <beckriley515@gmail.com>
2026-04-01 15:03:06 -0600

    Resolved build warning in UserInterface. Altered the style of buttons.

--- src/QuickFile/App.cs
+++ src/QuickFile/App.cs
@@ -11,13 +11,24 @@ public class App : Application
     public override void Initialize()
     {
         Styles.Add(new FluentTheme());
-        //Button Styles
+        //Text Styles
         Styles.Add(new Style(x => x.OfType<TextBlock>())
         {
             Setters = {
             new Setter(TextBlock.FontFamilyProperty, new FontFamily("SF Pro Display, Helvetica Neue, Arial")),
             new Setter(TextBlock.FontSizeProperty, 13.0),
             new Setter(TextBlock.ForegroundProperty, new SolidColorBrush(Color.Parse("#1C1C1E"))),
+            }
+        });
+        //Button Styles
+        Styles.Add(new Style(x => x.OfType<Button>())
+        {
+            Setters ={
+            new Setter(Button.CornerRadiusProperty, new CornerRadius(6)),
+            new Setter(Button.PaddingProperty, new Thickness(14,6)),
+            new Setter(Button.BackgroundProperty, new SolidColorBrush((Color.Parse("#007AFF")))),
+            new Setter(Button.ForegroundProperty, Brushes.White),
+            new Setter(Button.BorderThicknessProperty, new Thickness(0))
             }
         });
     }
--- src/QuickFile/UserInterface.cs
+++ src/QuickFile/UserInterface.cs
@@ -1,6 +1,5 @@
 using Avalonia.Controls;
 using Avalonia.Interactivity;
-using Avalonia.Controls;
 using Avalonia.Media;
 using Avalonia.Styling;
 using Avalonia.Platform;
